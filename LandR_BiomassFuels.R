38d31bfea3d3e084da3212955d3f7768d187d4c7
CeresBarros <cbarros@mail.ubc.ca>
2018-08-14 18:09:11 -0700

    LANDIS II extension version added to metadata

--- LandR_BiomassFuels.R
+++ LandR_BiomassFuels.R
@@ -4,7 +4,7 @@
 # in R packages. If exact location is required, functions will be: sim$<moduleName>$FunctionName
 defineModule(sim, list(
   name = "LandR_BiomassFuels",
-  description = "SpaDES version of the LANDIS-II Dynamic Biomass Fuels Extention", #"insert module description here",
+  description = "SpaDES version of the LANDIS-II Dynamic Biomass Fuels Extention v2.2 - 15 Jun 2017", #"insert module description here",
   keywords = c("fire fuels", "fuel type", "LANDIS", "LandR"), 
   authors = person("Ceres", "Barros", email = "cbarros@mail.ubc.ca", 
                    role = c("aut", "cre")),
