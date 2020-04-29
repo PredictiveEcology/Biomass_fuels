ec889eb12ca10e2d24e13f3ec7d1c247157ab4eb
CeresBarros <cbarros@mail.ubc.ca>
2020-04-29 15:54:15 +0000

    compare projs with compareCRS

--- Biomass_fuels.R
+++ Biomass_fuels.R
@@ -509,7 +509,7 @@ calcFuelTypes <- function(sim) {
                                  datatype = "INT2U", overwrite = TRUE)
     }
 
-    if (!identical(crs(sim$studyArea), crs(sim$rasterToMatch))) {
+    if (!compareCRS(sim$studyArea, sim$rasterToMatch)) {
       warning(paste0("studyArea and rasterToMatch projections differ.\n",
                      "studyArea will be projected to match rasterToMatch"))
       sim$studyArea <- spTransform(sim$studyArea, crs(sim$rasterToMatch))
